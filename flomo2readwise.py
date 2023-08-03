ffb227c21855de732eb0bed4ecf888f979d1f23d
xiaoxiang52 <127318734+xiaoxiang52@users.noreply.github.com>
2023-08-03 19:29:16 +0800

    fix: mul count upload

--- flomo2readwise.py
+++ flomo2readwise.py
@@ -1,12 +1,14 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 from flomoDatabase import FlomoDatabase
 from readwise import Readwise
 from logger import loguru_logger
-
-NOTION_INTEGRATION_TOKEN	= os.environ['NOTION_INTEGRATION_TOKEN']
-NOTION_DATABASE_ID			= os.environ['NOTION_DATABASE_ID']
-READWISE_ACCESS_TOKEN		= os.environ['READWISE_ACCESS_TOKEN']
+# os.environ['NOTION_INTEGRATION_TOKEN'] = "secret_sbEgXxZiwDwkTvoRW80il0CUhOL4Ns1eWr25y1temRJ"
+# os.environ['NOTION_DATABASE_ID'] = "f323f02767944b14ab4fb33f9eb50d76"
+# os.environ['READWISE_ACCESS_TOKEN'] = "71FYCeD5zI0CHATsKofjiHkcRItkMfypRECZRHHZeDXlmuW6se"
+NOTION_INTEGRATION_TOKEN = os.environ['NOTION_INTEGRATION_TOKEN']
+NOTION_DATABASE_ID = os.environ['NOTION_DATABASE_ID']
+READWISE_ACCESS_TOKEN = os.environ['READWISE_ACCESS_TOKEN']
 
 # Only sync new memos by managing a last sync time
 # Both Github Actions and Notion API are in UTC time zone
@@ -14,44 +16,54 @@ last_sync_time_file = 'last_sync_time.txt'
 # Save all logs to a same file
 logger = loguru_logger('flomo2readwise')
 
-def get_last_sync_time():
-	if not os.path.exists(last_sync_time_file):
-		return None
-	with open(last_sync_time_file, 'r') as f:
-		return datetime.fromisoformat(f.read())
 
-def update_last_sync_time():
-	update_time = datetime.now()	# UTC time on Github Actions
-	with open(last_sync_time_file, 'w') as f:
-		f.write(str(update_time))
-	return update_time
+def get_last_sync_time():
+    if not os.path.exists(last_sync_time_file):
+        return None
+    with open(last_sync_time_file, 'r') as f:
+        return datetime.fromisoformat(f.read().replace("\n", ""))
+
+
+def update_last_sync_time(sync_time=None):
+    if not sync_time:
+        update_time = datetime.now()  # UTC time on Github Actions
+    else:
+        dt_obj = datetime.strptime(
+            '2023-07-31T14:44:00.000Z', '%Y-%m-%dT%H:%M:%S.%fZ')
+        update_time = dt_obj.strftime('%Y-%m-%dT%H:%M:%S.%f')
+    with open(last_sync_time_file, 'w') as f:
+        f.write(str(update_time))
+    return update_time
+
+
+readwise = Readwise(READWISE_ACCESS_TOKEN, logger)
+
+
+def sync_callback(flomo_memos):
+    global readwise
+    last_hl = readwise.create_highlights_from_memos(flomo_memos)
+    update_time = update_last_sync_time(last_hl["highlighted_at"])
+    logger.log('Update last sync time:', update_time)
+
 
 def sync_flomo_to_readwise():
-	# Get last sync time
-	last_sync_time = get_last_sync_time()
-	if last_sync_time:
-		logger.log('Last sync time:', last_sync_time)
-	else:
-		logger.log('First sync')
+    last_sync_time = get_last_sync_time()
+    if last_sync_time:
+        logger.log('Last sync time:', last_sync_time)
+    else:
+        logger.log('First sync')
 
-	# Fetch flomo memos
-	flomo_database = FlomoDatabase(NOTION_INTEGRATION_TOKEN, NOTION_DATABASE_ID, logger)
-	flomo_memos = flomo_database.fetch_flomo_memos(last_sync_time=last_sync_time)
-	logger.log('Number of flomo memos to sync:', len(flomo_memos))
+    # Fetch flomo memos
+    flomo_database = FlomoDatabase(
+        NOTION_INTEGRATION_TOKEN, NOTION_DATABASE_ID, logger)
+    flomo_memos = flomo_database.fetch_flomo_memos(
+        sync_callback,
+        last_sync_time=last_sync_time)
+    logger.log('Number of flomo memos to sync:', len(flomo_memos))
 
-	if len(flomo_memos) > 0:
-		# Sync flomo memos to Readwise
-		readwise = Readwise(READWISE_ACCESS_TOKEN, logger)
-		readwise.create_highlights_from_memos(flomo_memos)
-		logger.log('Finished syncing flomo memos to Readwise')
-	else:
-		logger.log('No flomo memos to sync')
+    logger.log('Finished')
+    logger.log('')
 
-	# Update last sync time
-	update_time = update_last_sync_time()
-	logger.log('Update last sync time:', update_time)
-	logger.log('Finished')
-	logger.log('')
 
 if __name__ == '__main__':
-	sync_flomo_to_readwise()
+    sync_flomo_to_readwise()
--- readwise.py
+++ readwise.py
@@ -2,93 +2,106 @@ import requests
 from datetime import datetime
 from dataclasses import dataclass
 from typing import List, Dict, Optional
+from tenacity import retry, wait_exponential, stop_after_attempt
 
-## Readwise will first check title/author/source_type to determine if it is a new book
-## then check text to determine if it is a new highlight
-## if not new, it will update it (e.g update category/highlighted_at)
+# Readwise will first check title/author/source_type to determine if it is a new book
+# then check text to determine if it is a new highlight
+# if not new, it will update it (e.g update category/highlighted_at)
 
 # a class for a readwise highlight
+
+
 @dataclass
 class ReadwiseHighlight:
-	text: 			str
-	title:			Optional[str]	= None
-	author:			Optional[str]	= None
-	image_url:		Optional[str]	= None
-	source_url:		Optional[str]	= None
-	source_type:	Optional[str]	= None
-	category:		Optional[str]	= None	# books, articles, podcasts, tweets
-	note:			Optional[str]	= None	# create tags: '.tag1 .tag2'
-	location:		Optional[int]	= None
-	location_type:	Optional[str]	= None	# page, order, time_offset
-	highlighted_at:	Optional[str]	= None	# ISO 8601 format, e.g. 2020-07-14T20:11:24+00:00
-	highlight_url:	Optional[str]	= None
+    text: 			str
+    title:			Optional[str] = None
+    author:			Optional[str] = None
+    image_url:		Optional[str] = None
+    source_url:		Optional[str] = None
+    source_type:	Optional[str] = None
+    category:		Optional[str] = None  # books, articles, podcasts, tweets
+    note:			Optional[str] = None  # create tags: '.tag1 .tag2'
+    location:		Optional[int] = None
+    location_type:	Optional[str] = None  # page, order, time_offset
+    # ISO 8601 format, e.g. 2020-07-14T20:11:24+00:00
+    highlighted_at:	Optional[str] = None
+    highlight_url:	Optional[str] = None
 
-	# get a dict with non-None fields
-	def get_dict(self):
-		return {k: v for k, v in self.__dict__.items() if v is not None}
+    # get a dict with non-None fields
+    def get_dict(self):
+        return {k: v for k, v in self.__dict__.items() if v is not None}
+
 
 class Readwise:
-	def __init__(self, access_token, logger):
-		self.access_token = access_token
-		self.headers = {'Authorization': 'Token ' + access_token}
-		self.highlight_create_url = 'https://readwise.io/api/v2/highlights/'
-		self.logger = logger
+    def __init__(self, access_token, logger):
+        self.access_token = access_token
+        self.headers = {'Authorization': 'Token ' + access_token}
+        self.highlight_create_url = 'https://readwise.io/api/v2/highlights/'
+        self.logger = logger
 
-	""" Create highlights in Readwise """
+    """ Create highlights in Readwise """
 
-	def create_highlights(self, highlights: List[Dict]):
-		res = requests.post(
-			url=self.highlight_create_url,
-			headers=self.headers,
-			json={"highlights": highlights}
-		)
-		if res.status_code != 200:
-			self.logger.log('Failed to create highlights')
-			self.logger.log(f'Error code: {res.status_code}')
-			self.logger.log(f'Error message: {res.text}')
-			raise Exception('Failed to create highlights')
-	
-	def create_highlights_from_memos(self, memos: List[Dict]):
-		highlights = self.convert_memos_to_highlights(memos)
-		self.create_highlights(highlights)
+    def create_highlights(self, highlights: List[Dict]):
+        self.logger.log(f'Error message: {highlights}')
+        res = requests.post(
+            url=self.highlight_create_url,
+            headers=self.headers,
+            json={"highlights": highlights}
+        )
+        if res.status_code != 200:
+            self.logger.log('Failed to create highlights')
+            self.logger.log(f'Error code: {res.status_code}')
+            self.logger.log(f'Error message: {res.text}')
+            raise Exception('Failed to create highlights')
 
-	""" flomo memo -> Readwise highlight """
+    @retry(wait=wait_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(5))
+    def create_highlights_from_memos(self, memos: List[Dict]):
+        highlights = self.convert_memos_to_highlights(memos)
+        self.create_highlights(highlights)
+        return highlights[-1]
 
-	def convert_memos_to_highlights(self, memos: List[Dict]) -> List[Dict]:
-		highlights = []
-		# convert
-		for memo in memos:
-			highlights.append(self.convert_memo_to_highlight(memo))
-		# sort
-		highlights = self.sort_highlights_by_time(highlights)
-		return highlights
+    """ flomo memo -> Readwise highlight """
 
-	def convert_memo_to_highlight(self, memo: dict) -> Dict:
-		hl = ReadwiseHighlight(text=memo['text'])
-		if 'author' in memo:	hl.author = memo['author']
-		if 'category' in memo:	hl.category = memo['category']
-		if 'title' in memo:		hl.title = memo['title']
-		else:
-			title = memo['text'].split('\n')[0]
-			if len(title) > 20: title = title[:20] + '...'
-			hl.title = title
-		if len(hl.title) <= 0:
-			hl.title = '未知标题' # ignore this title 
-		hl.note = self.convert_tags_to_note(memo['tags'])
-		hl.highlight_url = memo['flomo_url']
-		hl.highlighted_at = self.convert_memotime_to_iso8601(memo['edit_time'])
-		hl.source_type = 'flomo-Notion'
-		return hl.get_dict()
+    def convert_memos_to_highlights(self, memos: List[Dict]) -> List[Dict]:
+        highlights = []
+        # convert
+        for memo in memos:
+            highlights.append(self.convert_memo_to_highlight(memo))
+        # sort
+        highlights = self.sort_highlights_by_time(highlights)
+        return highlights
 
-	def convert_tags_to_note(self, tags: List[str]) -> str:
-		return ' '.join([f'.{tag}' for tag in tags])
+    def convert_memo_to_highlight(self, memo: dict) -> Dict:
+        hl = ReadwiseHighlight(text=memo['text'])
+        if 'author' in memo:
+            hl.author = memo['author']
+        if 'category' in memo:
+            hl.category = memo['category']
+        if 'title' in memo:
+            hl.title = memo['title']
+        else:
+            title = memo['text'].split('\n')[0]
+            if len(title) > 20:
+                title = title[:20] + '...'
+            hl.title = title
+        hl.note = self.convert_tags_to_note(memo['tags'])
+        hl.highlight_url = memo['flomo_url']
+        hl.highlighted_at = self.convert_memotime_to_iso8601(memo['edit_time'])
+        hl.source_type = 'flomo-Notion'
+        if not hl.title:
+            hl.title = "无标题"
+        return hl.get_dict()
 
-	def convert_memotime_to_iso8601(self, memotime: str) -> str:
-		# memotime format: '2023-04-17T00:00:00.000Z'
-		# iso8601 format: '2023-04-17T00:00:00+00:00'
-		return memotime.replace('Z', '+00:00')
+    def convert_tags_to_note(self, tags: List[str]) -> str:
+        return ' '.join([f'.{tag}' for tag in tags])
 
-	def sort_highlights_by_time(self, highlights: List[Dict]) -> List[Dict]:
-		# time format: 'YYYY-MM-DDTHH:MM:SS+00:00'
-		highlights.sort(key=lambda x: datetime.fromisoformat(x['highlighted_at']))
-		return highlights
+    def convert_memotime_to_iso8601(self, memotime: str) -> str:
+        # memotime format: '2023-04-17T00:00:00.000Z'
+        # iso8601 format: '2023-04-17T00:00:00+00:00'
+        return memotime.replace('Z', '+00:00')
+
+    def sort_highlights_by_time(self, highlights: List[Dict]) -> List[Dict]:
+        # time format: 'YYYY-MM-DDTHH:MM:SS+00:00'
+        highlights.sort(
+            key=lambda x: datetime.fromisoformat(x['highlighted_at']))
+        return highlights
